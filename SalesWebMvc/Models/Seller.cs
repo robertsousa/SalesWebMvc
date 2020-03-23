791739f9ddcfc212d9d05a80f7f8fe73850e000c
Roberto Sousa <robert6257@gmail.com>
2020-03-23 11:26:11 -0300

    Foreign key not null (Referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,21 +8,23 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
-        public string Nme { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
+        public int DepartmentId { get; set; }
+
         public Seller()
         {
         }
 
-        public Seller(int id, string nme, string email, DateTime birthDate, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
-            Nme = nme;
+            Name = name;
             Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
