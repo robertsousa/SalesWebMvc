3b3e7e68d9d2f516ab52403a6ac7257d34cf3fb3
Roberto Sousa <robert6257@gmail.com>
2020-03-23 14:11:23 -0300

    Returning custom error page

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,7 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models.ViewModels;
+using SalesWebMvc.Models;
+
 
 namespace SalesWebMvc.Controllers
 {
